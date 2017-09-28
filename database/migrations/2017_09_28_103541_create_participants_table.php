f405bf9da289f5712c0eb523e75be3bdcb0930f2
Alessandro Aussems <alessandrossms@gmail.com>
2017-09-28 12:41:10 +0200

    CreatedParticipantsTable

---
+++ database/migrations/2017_09_28_103541_create_participants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('adress');
+            $table->string('city');
+            $table->string('question');
+            $table->string('ip');
+            $table->boolean('enabled');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
