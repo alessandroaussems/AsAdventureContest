9b150e2b0a3f333b58d4f04909cb974f874dd6cb
Alessandro Aussems <alessandrossms@gmail.com>
2017-10-02 19:48:48 +0200

    AddedCronToKernel
    -Cron NOT running

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        'App\Console\Commands\Winner',
         //
     ];
 
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('pick:winner')->dailyAt('19:47');
     }
 
     /**
