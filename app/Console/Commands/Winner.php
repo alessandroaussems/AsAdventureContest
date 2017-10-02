66c136777d476f3371ff748786548fb9d3c7127d
Alessandro Aussems <alessandrossms@gmail.com>
2017-10-02 19:41:53 +0200

    CreatedPickWinnerCommand

---
+++ app/Console/Commands/Winner.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Period;
+use App\Participant;
+use App\Winners;
+
+class Winner extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pick:winner';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pick a winner at the end of the period';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $periods = Period::all();
+        $now=Carbon::today()->toDateString();
+        foreach ($periods as $key => $value)
+        {
+            if($now == $value->enddate)
+            {
+                $period=$value->id;
+                if(Winners::where('period',$value->id)->get()->isEmpty())
+                {
+                    echo "NEW WINNER";
+                    $winnerofparticipants=Participant::where("question","true")->orderByRaw("RAND()")
+                        ->take(1)
+                        ->get();
+                    $winner = new Winners();
+                    $winner->participant     = $winnerofparticipants[0]->id;
+                    $winner->period          = $period;
+                    $winner->save();
+                }
+            }
+        }
+    }
+}
---
+++ database/migrations/2017_10_02_165622_create_winner_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWinnerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('winners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("participant");
+            $table->integer("period");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('winners');
+    }
+}
