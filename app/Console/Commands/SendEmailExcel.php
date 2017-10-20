d9f9cf44e47e88fc29068f3d7a8f65cfea3a502e
Alessandro Aussems <alessandrossms@gmail.com>
2017-10-20 14:32:34 +0200

    SendExcelJoinedToday

--- app/Console/Commands/SendEmailExcel.php
+++ app/Console/Commands/SendEmailExcel.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 use App\Participant;
 use Illuminate\Console\Command;
 use Excel;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 class SendEmailExcel extends Command
 {
@@ -42,11 +43,11 @@ class SendEmailExcel extends Command
 
             $excel->sheet("Participants", function($sheet) {
 
-                $sheet->fromArray(Participant::where('enabled',1)->get(), null, 'A1', false, false);
+                $sheet->fromArray(Participant::where('enabled',1)->where('date_participated',Carbon::today())->get(), null, 'A1', false, false);
 
             });
         })->store('xls', false, true)["full"];
-        Mail::raw("In attachment excelfile of participants ", function($message)
+        Mail::raw("In attachment excelfile of participants who joined today!", function($message)
         {
             $message->subject('ExcelFile Participants!');
             $message->from('no-reply@asadventurecontest.be', 'As Adventure Contest');
