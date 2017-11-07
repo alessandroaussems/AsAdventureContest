d4940ab4738c085fadea1b307334b19cdd27d25f
Alessandro Aussems <alessandrossms@gmail.com>
2017-11-07 16:25:14 +0100

    SeederAddsPeriods

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,28 @@ class DatabaseSeeder extends Seeder
         DB::table('adminmail')->insert([
             'email' => 'asadventurecontest@alessandro.aussems.mtantwerp.eu',
         ]);
+        DB::table('periods')->insert([
+            [
+            'title' => 'Period 1',
+            'startdate' => date('Y-m-d','1506816000'),
+            'enddate' => date('Y-m-d','1509408000')
+            ],
+            [
+            'title' => 'Period 2',
+            'startdate' => date('Y-m-d','1509494400'),
+            'enddate' => date('Y-m-d','1512000000')
+            ],
+            [
+            'title' => 'Period 3',
+            'startdate' => date('Y-m-d','1512086400'),
+            'enddate' => date('Y-m-d','1514678400')
+            ],
+            [
+            'title' => 'Period 4',
+            'startdate' => date('Y-m-d','1514764800'),
+            'enddate' => date('Y-m-d','1517356800')
+            ]
+            ]
+        );
     }
 }
