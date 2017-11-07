27aa757e3c1c62767388681696299e63d2bea7fd
Alessandro Aussems <alessandrossms@gmail.com>
2017-11-07 14:21:55 +0100

    AddedSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => 'asadventurecontest@alessandro.aussems.mtantwerp.eu',
+            'password' => bcrypt('asadmin1'),
+        ]);
     }
 }
